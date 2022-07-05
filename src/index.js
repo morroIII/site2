552dfd7af976c907dba2803ba054c7074486904f
MorroIII <luetrusnn@gmail.com>
2022-07-05 13:32:26 +0500

    add select

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './assets/style/index.scss'
-
+import Select from "./components/select";
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
         init();
@@ -10,4 +10,17 @@ if (document.readyState === 'loading') {
 
 function init(){
     console.log('init')
+
+    const sortEl = document.getElementById('sort')
+    
+
+    const sortCallback = (item) => {
+        console.log(item)
+    }
+
+    new Select({
+        el: sortEl,
+        onChange: sortCallback,
+        cookieName: 'catalog-sort'
+    })
 }
