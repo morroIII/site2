7debf6117fa777f840c7ae295a32a82eb5a8ca6e
MorroIII <luetrusnn@gmail.com>
2022-07-05 20:05:20 +0500

    add filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './assets/style/index.scss'
 import Select from "./components/select";
+import Accordion from "@/components/accordion";
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
         init();
@@ -9,7 +10,7 @@ if (document.readyState === 'loading') {
 }
 
 function init(){
-    console.log('init')
+
 
     const sortEl = document.getElementById('sort')
     
@@ -23,4 +24,30 @@ function init(){
         onChange: sortCallback,
         cookieName: 'catalog-sort'
     })
+
+    function filter() {
+        const accordions = []
+
+        const accordionsEl = document.querySelectorAll('[data-accordion]')
+        accordionsEl.forEach(accordion => {
+            accordions.push(new Accordion(accordion))
+        })
+
+        const hideFiltersBtn = document.querySelector('[data-filters-open]')
+        const openFiltersBtn = document.querySelector('[data-filters-hide]')
+
+        hideFiltersBtn.addEventListener('click', () => {
+            accordions.forEach(accordion => {
+                accordion.hide()
+            })
+        })
+
+        openFiltersBtn.addEventListener('click', () => {
+            accordions.forEach(accordion => {
+                accordion.open()
+            })
+        })
+    }
+
+    filter()
 }
